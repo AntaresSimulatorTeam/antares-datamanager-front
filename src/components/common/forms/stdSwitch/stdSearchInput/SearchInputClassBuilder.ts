8becf3f20c1684ed383bb43a9f7c41f340e149cb
vargastat <tatiana.vargas@rte-france.com>
2024-11-07 17:07:00 +0100

    feat: StdSearchInput updated

---
+++ src/components/common/forms/stdSwitch/stdSearchInput/SearchInputClassBuilder.ts
@@ -0,0 +1,42 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { clsx } from 'clsx';
+import { SearchInputSize, SearchVariant } from './StdSearchInput';
+
+export const INPUT_WRAPPER =
+  'rounded-0.5 group inline-flex w-full flex-row items-center gap-1 justify-center rounded border';
+export const INPUT_CLASSES =
+  'w-full h-2.5 bg-transparent outline-none [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-search-cancel-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none text-gray-900 placeholder:text-gray-500 ';
+export const LABEL_CLASSES = 'mb-0.25 h-auto w-auto overflow-ellipsis px-0.5 text-gray-700';
+export const VARIANT_CLASSES = {
+  filled:
+    'hover:placeholder:text-gray-700 border-gray-w border-b-gray-400 bg-gray-100  focus-within:border-b-primary-600 hover:border-b-gray-500 hover:bg-gray-200  focus:bg-gray-100 [&>*]:hover:placeholder:text-gray-700 [&>*]:focus:text-gray-900',
+  outlined:
+    'hover:placeholder:text-gray-700 rounded border-gray-400 focus-within:border-primary-600 hover:bg-gray-100 focus:border-primary-600',
+};
+export const DEFAULT_CLEAR_BUTTON_CLASS = 'flex flex-row items-start justify-center';
+export const INVISIBLE_CLEAR_BUTTON_CLASSES = 'invisible h-2.5';
+export const VARIANT_DISABLED_CLASSES = 'hover:bg-gray-200 cursor-not-allowed bg-gray-200 text-gray-500 ';
+
+export const SIZE_CLASSES = {
+  small: 'px-1 py-0.25',
+  medium: 'px-1.5 py-0.75',
+};
+
+export const searchClassBuilder = (variant: SearchVariant, disabled: boolean, size: SearchInputSize) => ({
+  inputClasses: clsx(INPUT_CLASSES, disabled && VARIANT_DISABLED_CLASSES),
+  inputWrapperClass: clsx(
+    INPUT_WRAPPER,
+    VARIANT_CLASSES[variant],
+    SIZE_CLASSES[size],
+    disabled && VARIANT_DISABLED_CLASSES,
+  ),
+  labelClass: clsx(LABEL_CLASSES),
+});
+
+export const clearClassBuilder = (shouldHideClearButton: boolean) =>
+  shouldHideClearButton ? INVISIBLE_CLEAR_BUTTON_CLASSES : DEFAULT_CLEAR_BUTTON_CLASS;
---
+++ src/components/common/forms/stdSwitch/stdSearchInput/tests/SearchInput.test.tsx
@@ -0,0 +1,86 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { noop } from '@/shared/utils/common/defaultUtils';
+import { StdIconId } from '@/shared/utils/common/mappings/iconMaps';
+import { fireEvent, render, screen } from '@testing-library/react';
+import StdSearchInput from '../StdSearchInput';
+
+const TEST_ICON = StdIconId.Search;
+
+const CLOSE_BUTTON = StdIconId.Close;
+const INPUT_ID = 'search-me-better-than-google';
+const COMMON_PLACEHOLDER = 'Veuillez effectuer votre recherche';
+const COMMON_LABEL = 'Alpha';
+const COMMON_DEFAULT_VALUE = 'Where are the Metrix';
+const COMMON_ROLE = 'searchbox';
+
+describe('Search Input', () => {
+  it('renders SearchInput Component', () => {
+    render(<StdSearchInput onSearch={noop} />);
+    expect(screen.getByTitle(TEST_ICON)).toBeInTheDocument();
+  });
+
+  it('It shows label when sent', () => {
+    render(<StdSearchInput label={COMMON_LABEL} placeHolder={COMMON_PLACEHOLDER} onSearch={noop} />);
+    expect(screen.getByLabelText(COMMON_LABEL)).toBeInTheDocument();
+  });
+
+  it('It shows The Label', () => {
+    render(<StdSearchInput placeHolder={COMMON_PLACEHOLDER} label={COMMON_LABEL} onSearch={noop} />);
+    expect(screen.getByLabelText(COMMON_LABEL)).toBeInTheDocument();
+  });
+
+  it('It is focusable when not disabled', () => {
+    render(<StdSearchInput placeHolder={COMMON_PLACEHOLDER} label={COMMON_LABEL} id={INPUT_ID} onSearch={noop} />);
+    const inputText = screen.getByLabelText(COMMON_LABEL);
+    inputText.focus();
+    expect(screen.getByRole(COMMON_ROLE)?.matches(':focus')).toBeTruthy();
+  });
+  it('It is not focusable when Disabled', () => {
+    render(
+      <StdSearchInput placeHolder={COMMON_PLACEHOLDER} label={COMMON_LABEL} id={INPUT_ID} disabled onSearch={noop} />,
+    );
+    const inputText = screen.getByLabelText(COMMON_LABEL);
+    inputText.focus();
+    expect(screen.getByRole(COMMON_ROLE)?.matches(':focus')).toBeFalsy();
+  });
+  it('It does not show clear button when there is no text', () => {
+    render(<StdSearchInput placeHolder={COMMON_PLACEHOLDER} label={COMMON_LABEL} onSearch={noop} />);
+
+    expect(screen.getByTitle(CLOSE_BUTTON)).not.toBeVisible();
+  });
+  it('It shows clear button when text is enterred', () => {
+    render(
+      <StdSearchInput
+        placeHolder={COMMON_PLACEHOLDER}
+        label={COMMON_LABEL}
+        defaultValue={COMMON_DEFAULT_VALUE}
+        onSearch={noop}
+      />,
+    );
+    expect(screen.getByTitle(CLOSE_BUTTON)).toBeInTheDocument();
+  });
+  it('It clears input when clear button clicked', () => {
+    render(
+      <StdSearchInput
+        placeHolder={COMMON_PLACEHOLDER}
+        label={COMMON_LABEL}
+        defaultValue={COMMON_DEFAULT_VALUE}
+        onSearch={noop}
+      />,
+    );
+    const clearBtn = screen.getByTitle(CLOSE_BUTTON);
+    fireEvent(
+      clearBtn,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }),
+    );
+    expect(screen.getByDisplayValue('')).toBeInTheDocument();
+  });
+});
