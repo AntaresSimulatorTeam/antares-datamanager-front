1b2287e691d9ee3ff94bcb9edc41d544f6ef39fa
vargastat <tatiana.vargas@rte-france.com>
2024-12-02 10:01:58 +0100

    fix: exclude test

--- vite.config.ts
+++ vite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['lcov', 'text'],
-      exclude: ['**/node_modules/**', 'src/*.test.ts', 'src/shared/types/common/**', 'src/components/common/forms/**'],
+      exclude: ['src/*.test.ts', 'src/shared/**', 'src/components/common/**'],
     },
   },
   server: {
