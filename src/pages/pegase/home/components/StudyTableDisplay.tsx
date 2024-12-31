b86c5bf0ce5f0578e7e6d02aa6b9fda8588dc15c
MOUAD EL AZAAR <mouad.elazaar_externe@rte-france.com>
2024-12-31 18:21:42 +0100

    feat: create and duplicate study

--- src/pages/pegase/home/components/StudyTableDisplay.tsx
+++ src/pages/pegase/home/components/StudyTableDisplay.tsx
@@ -5,18 +5,17 @@
  */
 
 import { useState } from 'react';
+import StdSimpleTable from '@/components/common/data/stdSimpleTable/StdSimpleTable';
 import { StudyDTO } from '@/shared/types/index';
+import getStudyTableHeaders from './StudyTableHeaders';
 import { addSortColumn, useNewStudyModal } from './StudyTableUtils';
 import StudiesPagination from './StudiesPagination';
 import { RowSelectionState } from '@tanstack/react-table';
-import { useStudyTableDisplay } from './useStudyTableDisplay';
-import { RdsButton } from 'rte-design-system-react';
-
-import getStudyTableHeaders from './StudyTableHeaders';
 import { StudyStatus } from '@/shared/types/common/StudyStatus.type';
-import StdSimpleTable from '@/components/common/data/stdSimpleTable/StdSimpleTable';
-import StdModal from '@/components/common/layout/stdModal/StdModal';
+import { useStudyTableDisplay } from './useStudyTableDisplay';
 import { useTranslation } from 'react-i18next';
+import StudyCreationModal from '../../studies/StudyCreationModal';
+import {RdsButton} from "rte-design-system-react";
 
 interface StudyTableDisplayProps {
   searchStudy: string | undefined;
@@ -30,12 +29,14 @@ const StudyTableDisplay = ({ searchStudy, projectId }: StudyTableDisplayProps) =
   const [isHeaderHovered, setIsHeaderHovered] = useState<boolean>(false);
   const { isModalOpen, toggleModal } = useNewStudyModal();
   const { t } = useTranslation();
+  const [selectedStudy, setSelectedStudy] = useState<StudyDTO | null>(null);
 
   const handleSort = (column: string) => {
     const newSortOrder = sortByState[column] === 'asc' ? 'desc' : 'asc';
     setSortByState({ [column]: newSortOrder });
     setSortedColumn(column);
   };
+
   const handleHeaderHover = (hovered: boolean) => {
     setIsHeaderHovered(hovered);
   };
@@ -64,6 +65,12 @@ const StudyTableDisplay = ({ searchStudy, projectId }: StudyTableDisplayProps) =
   const isDuplicateActive = selectedStatus === StudyStatus.GENERATED;
   const isDeleteActive = selectedStatus === StudyStatus.ERROR || selectedStatus === StudyStatus.IN_PROGRESS;
 
+  const handleDuplicate = () => {
+    const selectedStudy = rows[Number.parseInt(selectedRowId || '-1')];
+    setSelectedStudy(selectedStudy);
+    toggleModal();
+  };
+
   return (
     <div>
       <div className="flex-1">
@@ -89,12 +96,7 @@ const StudyTableDisplay = ({ searchStudy, projectId }: StudyTableDisplayProps) =
         <div className="flex gap-2">
           {selectedRowId !== undefined ? (
             <>
-              <RdsButton
-                label="Duplicate"
-                onClick={() => console.log('duplicate')}
-                variant="outlined"
-                disabled={!isDuplicateActive}
-              />
+              <RdsButton label="Duplicate" onClick={handleDuplicate} variant="outlined" disabled={!isDuplicateActive} />
               <RdsButton
                 label="Delete"
                 onClick={() => console.log('Delete')}
@@ -106,17 +108,7 @@ const StudyTableDisplay = ({ searchStudy, projectId }: StudyTableDisplayProps) =
           ) : (
             <RdsButton label={t('home.@new_study')} onClick={toggleModal} />
           )}
-          {isModalOpen && (
-            <StdModal size="medium" onClose={toggleModal}>
-              <StdModal.Title>{t('home.@new_study')}</StdModal.Title>
-              <StdModal.Content>
-                <p>Here you can create a new study. Add your content here.</p>
-              </StdModal.Content>
-              <StdModal.Footer>
-                <RdsButton label="Close" onClick={toggleModal} />
-              </StdModal.Footer>
-            </StdModal>
-          )}
+          {isModalOpen && <StudyCreationModal isOpen={isModalOpen} onClose={toggleModal} study={selectedStudy} />}
         </div>
         <StudiesPagination count={count} intervalSize={intervalSize} current={current} onChange={setPage} />
       </div>
