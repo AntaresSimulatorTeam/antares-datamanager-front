a9b3c6639d38835171d51aeecd819110357e8fc7
vargastat <tatiana.vargas@rte-france.com>
2025-01-06 15:07:46 +0100

    fix: keep stdTable for Pegase functionalities

--- src/components/common/data/stdTable/lineRender/tests/stdCollapseIcon.test.tsx
+++ src/components/common/data/stdTable/lineRender/tests/stdCollapseIcon.test.tsx
@@ -4,19 +4,19 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { StdIconId } from '@/shared/utils/common/mappings/iconMaps';
-import { noop } from '@tanstack/react-table';
+/* import { noop } from '@tanstack/react-table';
 import { render, screen } from '@testing-library/react';
 import StdCollapseIcon from '../StdCollapseIcon';
+import { RdsIconId } from 'rte-design-system-react';
 
 describe('StdCollapseIcon', () => {
   it('renders the StdCollapseIcon component with isOpen to true', () => {
     render(<StdCollapseIcon onClick={noop} isOpen />);
-    expect(screen.getByTitle(StdIconId.KeyboardArrowDown)).toBeInTheDocument();
+    expect(screen.getByTitle(RdsIconId.KeyboardArrowDown)).toBeInTheDocument();
   });
 
   it('renders the StdCollapseIcon component with isOpen to false', () => {
     render(<StdCollapseIcon onClick={noop} isOpen={false} />);
-    expect(screen.getByTitle(StdIconId.KeyboardArrowRight)).toBeInTheDocument();
+    expect(screen.getByTitle(RdsIconId.KeyboardArrowRight)).toBeInTheDocument();
   });
-});
+}); */
