84d03dd158b2e786e48cb437c171c10bd544fb2c
MOUAD EL AZAAR <mouad.elazaar_externe@rte-france.com>
2025-02-14 10:57:46 +0100

    feat:refactoring authentification in local env

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,14 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { Suspense, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
 import './App.css';
-import ThemeHandler from './components/common/handler/ThemeHandler';
-import PegaseStar from './components/pegase/star/PegaseStar';
-import { UserSettingsContext } from '@/store/contexts/UserSettingsContext.tsx';
-import { THEME_COLOR } from '@/shared/types';
-import { menuBottomData, menuTopData } from './routes';
-import { PegaseToastContainer } from './shared/notification/containers';
-import ProjectDetails from './pages/pegase/projects/projectDetails/ProjectDetails';
-import StudyDetails from '@/pages/pegase/studies/studyDetails/studyDetails';
-import { RdsNavbar } from 'rte-design-system-react';
-import { navBarConfig } from '@/shared/const/navBarConfig';
-import { useTranslation } from 'react-i18next';
-import { translateMenuItemLabel } from '@/shared/utils/textUtils.ts';
-import { PEGASE_NAVBAR_ID } from '@/shared/constants.ts';
 import UserProvider from '@/store/contexts/UserProvider.tsx';
 import { AuthService } from '@/shared/services/authService.ts';
+import MainContent from '@/pages/pegase/home/components/MainContent';
 
 function App() {
-  const { t } = useTranslation();
-
+  const isAuthenticationActive = import.meta.env.VITE_IS_AUTHENTICATION_ACTIVE ?? true; // true if no variable is set
   useEffect(() => {
     const handleAuth = async () => {
       if (window.location.href.includes('code=')) {
@@ -36,33 +22,11 @@ function App() {
     void handleAuth();
   }, []);
 
-  return (
+  return isAuthenticationActive ? (
+    <MainContent />
+  ) : (
     <UserProvider initialValue={{ user: null }}>
-      <div className="flex h-screen w-screen dark:bg-gray-900 dark:text-gray-200">
-        <UserSettingsContext.Provider initialState={{ theme: THEME_COLOR.LIGHT }}>
-          <ThemeHandler />
-          <PegaseToastContainer />
-          <RdsNavbar
-            id={PEGASE_NAVBAR_ID}
-            topItems={translateMenuItemLabel(menuTopData, t)}
-            bottomItems={translateMenuItemLabel(menuBottomData, t)}
-            headerLink={'/'}
-            config={navBarConfig}
-          />
-          <div className="flex h-full w-full flex-col">
-            <PegaseStar />
-            <Suspense>
-              <Routes>
-                <Route path="/study/:studyName" element={<StudyDetails />} />
-                <Route path="/project/:projectName" element={<ProjectDetails />} />
-                {Object.entries([...menuBottomData, ...menuTopData]).map(([key, route]) => (
-                  <Route key={key} path={route.path} Component={route.component} />
-                ))}
-              </Routes>
-            </Suspense>
-          </div>
-        </UserSettingsContext.Provider>
-      </div>
+      <MainContent />
     </UserProvider>
   );
 }
---
+++ src/pages/pegase/home/components/MainContent.tsx
@@ -0,0 +1,55 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import PegaseStar from '@/components/pegase/star/PegaseStar';
+import ProjectDetails from '@/pages/pegase/projects/projectDetails/ProjectDetails';
+import StudyDetails from '@/pages/pegase/studies/studyDetails/studyDetails';
+import { menuBottomData, menuTopData } from '@/routes';
+import { UserSettingsContext } from '@/store/contexts/UserSettingsContext';
+import { THEME_COLOR } from '@/shared/types';
+import ThemeHandler from '@common/handler/ThemeHandler';
+import { PegaseToastContainer } from '@/shared/notification/containers';
+import { RdsNavbar } from 'rte-design-system-react';
+import { PEGASE_NAVBAR_ID } from '@/shared/constants';
+import { translateMenuItemLabel } from '@/shared/utils/textUtils';
+import { navBarConfig } from '@/shared/const/navBarConfig';
+import { useTranslation } from 'react-i18next';
+
+const MainContent = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex h-screen w-screen dark:bg-gray-900 dark:text-gray-200">
+      <UserSettingsContext.Provider initialState={{ theme: THEME_COLOR.LIGHT }}>
+        <ThemeHandler />
+        <PegaseToastContainer />
+        <RdsNavbar
+          id={PEGASE_NAVBAR_ID}
+          topItems={translateMenuItemLabel(menuTopData, t)}
+          bottomItems={translateMenuItemLabel(menuBottomData, t)}
+          headerLink={'/'}
+          config={navBarConfig}
+        />
+        <div className="flex h-full w-full flex-col">
+          <PegaseStar />
+          <Suspense>
+            <Routes>
+              <Route path="/study/:studyName" element={<StudyDetails />} />
+              <Route path="/project/:projectName" element={<ProjectDetails />} />
+              {Object.entries([...menuBottomData, ...menuTopData]).map(([key, route]) => (
+                <Route key={key} path={route.path} Component={route.component} />
+              ))}
+            </Routes>
+          </Suspense>
+        </div>
+      </UserSettingsContext.Provider>
+    </div>
+  );
+};
+
+export default MainContent;
