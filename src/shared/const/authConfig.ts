929f9ce2d1468c165563de263ce88d4f69e3ce1f
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-03-04 15:34:42 +0100

    fix: signinRedirectCallback 8

--- src/shared/const/authConfig.ts
+++ src/shared/const/authConfig.ts
@@ -5,17 +5,9 @@
  */
 
 import { getEnvVariables } from '@/envVariables.ts';
+import { UserManagerSettings } from 'oidc-client-ts';
 
-interface AuthConfig {
-  authority: string;
-  client_id: string;
-  redirect_uri: string;
-  scope: string;
-  maxExpiresIn: number;
-  post_logout_redirect_uri: string;
-}
-
-export const config: AuthConfig = {
+export const config: UserManagerSettings = {
   // eslint-disable-next-line camelcase
   client_id: getEnvVariables('VITE_OAUTH2_CLIENT_ID'),
   // eslint-disable-next-line camelcase
@@ -24,5 +16,6 @@ export const config: AuthConfig = {
   // eslint-disable-next-line camelcase
   post_logout_redirect_uri: getEnvVariables('VITE_OAUTH2_LOGOFF_REDIRECT_URL'),
   scope: 'openid email profile',
-  maxExpiresIn: 600,
+  revokeTokensOnSignout: true,
+  redirectMethod: 'replace',
 };
