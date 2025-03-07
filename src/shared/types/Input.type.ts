5206898cc4fe607a936cf977182ddb5d0ebd5ab1
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-03-07 18:38:42 +0100

    fix: use delete service to unlink trajectory to study

--- src/shared/types/Input.type.ts
+++ src/shared/types/Input.type.ts
@@ -4,7 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-interface SelectOption {
-  id: number | string;
+export interface SelectOption {
+  id: number;
   label: string;
 }
