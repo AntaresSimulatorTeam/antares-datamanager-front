9d5ab189e9f1aa5075b09292ef32479b21e82b33
melazaar <56682415+melazaar@users.noreply.github.com>
2025-04-18 10:24:42 +0200

    shwo error message in toaster (#104)
    Co-authored-by: MOUAD EL AZAAR <mouad.elazaar_externe@rte-france.com>

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feature/ANT_3040_about_page",
-  commit: "d50ce86",
-  buildTime: "2025-04-07T12:12:37.726Z"
+  branch: "develop",
+  commit: "719f620",
+  buildTime: "2025-04-17T18:21:47.200Z"
 };
