c493c04cc633c00e21bf5b449c1f8326316359c5
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-04-18 17:58:43 +0200

    fix: add skip button

--- src/components/banner/ContainerWithExpander.tsx
+++ src/components/banner/ContainerWithExpander.tsx
@@ -9,40 +9,43 @@ import { CardWithIconTitle } from '@common/layout/CardWithIconTitle.tsx';
 import { convertDataToItem } from '@/shared/utils/warningUtils.ts';
 
 interface Props<T> {
-  content: T[] | null;
+  content: T[];
 }
 
 export const ContainerWithExpander = <T,>({ content }: Props<T>) => {
   const { t } = useTranslation();
   const [isOpen, setIsOpen] = useState(false);
-  const color = content?.length === 0 ? 'gray' : 'orange';
 
   return (
-    <div className="flex w-full rounded border-gray-600 bg-gray-200 pl-2 shadow-2 sm:pl-1">
+    <div className={`flex ${isOpen ? 'h-1/3' : 'h-fit'} w-full rounded border-gray-600 bg-gray-200 py-1 pl-2 shadow-2`}>
       <div className="flex w-full max-w-fit flex-col pt-3 sm:pt-2">
         <button onClick={() => setIsOpen((prev) => !prev)}>
           <StdIcon name={isOpen ? StdIconId.KeyboardArrowUp : StdIconId.KeyboardArrowDown} />
         </button>
       </div>
-      <div className="flex w-full flex-col">
-        <div className="flex">
-          <div className="flex w-full items-center gap-4 p-2 sm:gap-2 sm:p-1">
-            <StdAvatar
-              initials={`${content?.length ?? '0'}`}
-              size="es"
-              backgroundColor={color}
-              fullname=""
-              textColor="white"
-            />
-            <RdsHeading title={t('studyDetails.@alerts')} size="m" />
-          </div>
+      <div className="flex h-full w-full flex-col items-center">
+        <div className="flex w-full items-center gap-4 p-2 sm:gap-2 sm:p-1">
+          <StdAvatar
+            initials={`${content?.length ?? '0'}`}
+            size="es"
+            backgroundColor={content?.length === 0 ? 'gray' : 'orange'}
+            fullname=""
+            textColor="white"
+          />
+          <RdsHeading title={t('studyDetails.@alerts')} size="m" />
         </div>
-        {!!content?.length && isOpen && (
+        {isOpen && (
           <VirtualizerList
             isOpen={isOpen}
             items={content}
-            renderItem={(contentItem, size, transform) => (
-              <CardWithIconTitle data={convertDataToItem(contentItem, t)} size={size} transform={transform} />
+            renderItem={(contentItem, size, transform, key) => (
+              <CardWithIconTitle
+                key={key}
+                data={convertDataToItem(contentItem, t)}
+                size={size}
+                transform={transform}
+                buttonLabel={t('studyDetails.@skip')}
+              />
             )}
           />
         )}
--- src/components/common/layout/CardWithIconTitle.tsx
+++ src/components/common/layout/CardWithIconTitle.tsx
@@ -1,6 +1,7 @@
 import { StdIconId } from '@/shared/utils/common/mappings/iconMaps.ts';
 import StdIcon from '@common/base/stdIcon/StdIcon.tsx';
 import { formatDateToDDMMYYYY } from '@/shared/utils/dateFormatter.ts';
+import { RdsButton, RdsIconId, RdsTextTooltip } from 'rte-design-system-react';
 
 export type CardDataType = {
   color: string;
@@ -17,25 +18,32 @@ type CardWithIconTitleProps = {
   data: CardDataType;
   size: string;
   transform: string;
+  buttonLabel: string;
 };
 
-export const CardWithIconTitle = ({ data, size, transform }: CardWithIconTitleProps) => (
+export const CardWithIconTitle = ({ data, size, transform, buttonLabel }: CardWithIconTitleProps) => (
   <div
-    style={{
-      width: size,
-      transform,
-    }}
-    className={`absolute left-0 top-0 rounded-lg bg-gray-100 shadow-2 ${data.colorBorder} p-2`}
+    style={{ width: size, transform }}
+    className={`absolute left-0 grid h-full rounded-lg bg-gray-100 shadow-2 ${data.colorBorder} p-2`}
   >
-    <div className="flex items-center gap-1">
-      <StdIcon name={data.icon} color={`${data.color}`} />
-      <span className={`${data.color} text-body-s sm:text-body-s`}>{data.title}</span>
+    <div className="flex items-center justify-between gap-1">
+      <div className="flex justify-between gap-1">
+        <StdIcon name={data.icon} color={`${data.color}`} />
+        <span className={`${data.color} text-body-s sm:text-body-s`}>{data.title}</span>
+      </div>
+      <div className="flex">
+        <RdsButton label={buttonLabel} icon={RdsIconId.KeyboardArrowRight} size={'extraSmall'} variant={'outlined'} />
+      </div>
     </div>
-    <div className="mt-1 flex justify-start sm:mt-0">
+    <div className="mt-1 flex">
       {data?.subtitle && <span className="text-body-s sm:text-body-s">{data.subtitle}</span>}
     </div>
-    <div className="body-l sm:body-m my-1 line-clamp-2 text-start text-gray-600 sm:my-0">
-      {data?.content && <span className="text-body-s sm:text-body-s">{data.content}</span>}
+    <div className="body-l sm:body-m my-1 line-clamp-2 self-stretch text-start text-gray-600">
+      {data?.content && (
+        <RdsTextTooltip text={data?.content ?? ''} offset={5} placement="top">
+          <span className="flex text-body-s">{data.content}</span>
+        </RdsTextTooltip>
+      )}
     </div>
     <div className="flex gap-2 text-gray-600">
       <div className="body-l sm:body-m flex items-center gap-1">
