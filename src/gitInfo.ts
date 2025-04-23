771b7b4995f35345af60910e9fefe4380550f857
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-04-23 18:51:34 +0200

    fix: commit

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3023_hyptothesis_table_display_error_warning_messages",
-  commit: "7a994ba",
-  buildTime: "2025-04-23T15:12:51.464Z"
+  commit: "c3eb447",
+  commitTime: "2025-04-23T16:50:32.345Z"
 };
