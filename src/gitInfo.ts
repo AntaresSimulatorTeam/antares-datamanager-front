e1233c15e91c838aea655ccd7784ebc708dc4fc2
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-04-25 15:52:55 +0200

    fix: git-info commit

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-2958_Display_load_hypothesis_table",
-  commit: "2c115f0",
-  buildTime: "2025-04-24T15:07:24.534Z"
+  commit: "8580b74",
+  commitTime: "2025-04-25T13:52:18.726Z"
 };
