3fe44b5ec8b93ef5f7a226b136fcb9c70fa6e342
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-05-06 16:30:06 +0200

    fix: unlink area trajectory to study should lead to load

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-2962_bottom_sticky",
-  commit: "aa1f4b8",
-  commitTime: "2025-05-06T14:16:13.066Z"
+  branch: "fix/ANT-2962_handle_area_change",
+  commit: "18903c4",
+  commitTime: "2025-05-06T14:24:53.459Z"
 };
