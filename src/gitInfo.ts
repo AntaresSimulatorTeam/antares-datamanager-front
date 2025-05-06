c8aacfd45aa0c6acf0b5b61b22a203e18491a14b
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-05-06 13:30:31 +0200

    Fix: study state type

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-2962_load_add_area_to_list",
-  commit: "7e2c9d7",
-  commitTime: "2025-05-06T09:29:52.061Z"
+  branch: "fix/ANT-2962_build",
+  commit: "a08aaf6",
+  commitTime: "2025-05-06T11:28:32.780Z"
 };
