36a914decd4027fc89e3c70ee90b8295f71cab49
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-05-15 16:51:07 +0200

    fix: ANT-2962 - Height break when add multiple areas

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-3023_hypothesis_table_display_warning_messages",
-  commit: "185e1a7",
-  commitTime: "2025-05-15T12:33:26.461Z"
+  branch: "develop",
+  commit: "4573b9f",
+  commitTime: "2025-05-15T13:48:27.065Z"
 };
