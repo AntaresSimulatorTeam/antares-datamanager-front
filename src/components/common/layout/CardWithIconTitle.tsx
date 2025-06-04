2183be609c6c2c17a9628e14f97b1c71e52796cd
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-04 18:11:42 +0200

    fix: prevent warning from being skippable when study is generated

--- src/components/common/layout/CardWithIconTitle.tsx
+++ src/components/common/layout/CardWithIconTitle.tsx
@@ -18,28 +18,30 @@ export const CardWithIconTitle = ({ data, size, transform, buttonLabel }: CardWi
   return (
     <div
       style={{ width: size, transform }}
-      className={`absolute left-0 flex h-full flex-col justify-start gap-1 rounded-lg border-b-4 bg-gray-100 shadow-2 ${!data.isAck ? 'border-transparent' : 'border-b-gray-600'} ${!data.isAck ? data.colorBorder : 'hover:border-b-gray-600'} p-2`}
+      className={`absolute left-0 flex h-full flex-col justify-start gap-1 rounded-lg border-b-4 bg-gray-100 shadow-2 ${!data.isAck || data.onClickItem == null ? 'border-transparent' : 'border-b-gray-600'} ${!data.isAck ? data.colorBorder : data.onClickItem == null ? 'border-transparent' : 'hover:border-b-gray-600'} p-2`}
     >
       <div className="flex items-start justify-between gap-1">
         <RdsTextTooltip text={data.title} offset={5} placement="top">
           <div className="line-clamp-1 text-ellipsis break-all text-start text-body-s">{data.title}</div>
         </RdsTextTooltip>
-        <div className="flex">
-          <ButtonWithStdIcon
-            label={buttonLabel}
-            icon={StdIconId.KeyboardArrowRight}
-            position="left"
-            size="extraSmall"
-            color={data.colorStatus}
-            variant="outlined"
-            disabled={data.isAck}
-            onClick={() => {
-              if (data.id != null && data.onClickItem && dispatch) {
-                void data.onClickItem(data.id, data.trajectoryType, data.trajectoryId, dispatch);
-              }
-            }}
-          />
-        </div>
+        {data.onClickItem != null && (
+          <div className="flex">
+            <ButtonWithStdIcon
+              label={buttonLabel}
+              icon={StdIconId.KeyboardArrowRight}
+              position="left"
+              size="extraSmall"
+              color={data.colorStatus}
+              variant="outlined"
+              disabled={data.isAck}
+              onClick={() => {
+                if (data.id != null && data.onClickItem && dispatch) {
+                  void data.onClickItem?.(data.id, data.trajectoryType, data.trajectoryId, dispatch);
+                }
+              }}
+            />
+          </div>
+        )}
       </div>
       {data?.content && (
         <div className="flex h-full text-ellipsis text-gray-600">
