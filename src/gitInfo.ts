b3a43ba5bf2d653dda69e7975fb5bc236087404b
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-05 11:43:02 +0200

    fix: area and links: error message display

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "fix/ANT-3317_LOAD_tooltip_and_error_message",
-  commit: "12145bd",
-  commitTime: "2025-06-06T08:33:09.040Z"
+  commit: "2183be6",
+  commitTime: "2025-06-05T09:42:11.846Z"
 };
