48118bbf73162d686e7e23160307eada8c8c96b2
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-06 16:23:31 +0200

    Fix: ANT-3325 - Notification error: set newest on top

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-3325_Erreur_display_notification",
-  commit: "7f5161c",
-  commitTime: "2025-06-06T13:53:06.543Z"
+  branch: "fix/ANT-3325_notification_alert_newest_on_top",
+  commit: "79ba481",
+  commitTime: "2025-06-06T14:19:27.971Z"
 };
