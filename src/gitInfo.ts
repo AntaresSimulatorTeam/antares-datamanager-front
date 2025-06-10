a59492743fe1327f6c824ee9916384c3144c4dfb
marlenetienne <marlene.etienne@hotmail.fr>
2025-06-10 10:44:31 +0200

    Fix/ant 3325 remove notification alert (#134)
    * Fix: ANT-3325 - Comment notify alert
    
    * fix: ANT-3325 - remove notification alert
    
    ---------
    
    Co-authored-by: marlenetienne <marlene.etienne_externe@rte-france.com>

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-3325_notification_alert_newest_on_top",
-  commit: "48118bb",
-  commitTime: "2025-06-06T14:33:04.314Z"
+  branch: "fix/ANT-3325-remove_notification_alert",
+  commit: "824ffd9",
+  commitTime: "2025-06-10T08:38:16.909Z"
 };
