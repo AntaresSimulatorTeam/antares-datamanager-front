d170fac821f6e3eb0208a600d1f4edeee59d9acb
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-11 17:27:37 +0200

    fix: git info

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3157_study_page_handle_back_navigation",
-  commit: "37914a1",
-  commitTime: "2025-06-11T15:05:09.403Z"
+  commit: "85aeaba",
+  commitTime: "2025-06-11T15:11:46.608Z"
 };
