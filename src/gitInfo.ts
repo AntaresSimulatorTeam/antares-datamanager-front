1c9c00f2656e52a044fb1ee946ca2b365b251413
vargastat <tatiana.vargas@rte-france.com>
2025-06-11 20:47:09 +0200

    package-lock.json commited

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-3262_Warning_wrong_label_when_skipped",
-  commit: "f0001b8",
-  commitTime: "2025-06-10T15:15:52.160Z"
+  branch: "fix/jenkins",
+  commit: "60f4713",
+  commitTime: "2025-06-11T18:45:52.751Z"
 };
