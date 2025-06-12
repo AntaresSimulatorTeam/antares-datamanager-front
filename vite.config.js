9fddf5f896b701351c6e915704ac4084a58a80b8
vargastat <130074062+vargastat@users.noreply.github.com>
2025-06-12 08:36:10 +0200

    fix devin miroir pour package-lock.json (#142)

--- vite.config.js
+++ vite.config.js
@@ -11,61 +11,59 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 var DEFAULT_PORT = 8080;
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    topLevelAwait({
-      // The export name of top-level await promise for each chunk module
-      promiseExportName: '__tla',
-      // The function to generate import names of top-level await promise in each chunk module
-      promiseImportName: function (i) {
-        return '__tla_'.concat(i);
-      },
-    }),
-  ],
-  build: {
-    target: 'esnext',
-    rollupOptions: {
-      output: {
-        format: 'es',
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+    plugins: [
+        react(),
+        topLevelAwait({
+            // The export name of top-level await promise for each chunk module
+            promiseExportName: '__tla',
+            // The function to generate import names of top-level await promise in each chunk module
+            promiseImportName: function (i) { return "__tla_".concat(i); },
+        }),
+    ],
+    build: {
+        target: 'esnext',
+        rollupOptions: {
+            output: {
+                format: 'es',
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+                manualChunks: function (id) {
+                    if (/envVariables.ts/.test(id)) {
+                        return 'envVariables';
+                    }
+                },
+            },
         },
-        manualChunks: function (id) {
-          if (/envVariables.ts/.test(id)) {
-            return 'envVariables';
-          }
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/testSetup.ts',
+        coverage: {
+            provider: 'v8',
+            reporter: ['lcov', 'text'],
+            exclude: ['src/*.test.ts', 'src/shared/**', 'src/components/common/**'],
         },
-      },
+        server: { deps: { inline: ['rte-design-system'] } },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/testSetup.ts',
-    coverage: {
-      provider: 'v8',
-      reporter: ['lcov', 'text'],
-      exclude: ['src/*.test.ts', 'src/shared/**', 'src/components/common/**'],
+    server: {
+        port: DEFAULT_PORT,
     },
-    server: { deps: { inline: ['rte-design-system'] } },
-  },
-  server: {
-    port: DEFAULT_PORT,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@common': path.resolve(__dirname, './src/components/common'),
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@common': path.resolve(__dirname, './src/components/common'),
+        },
     },
-  },
-  esbuild: {
-    supported: {
-      'top-level-await': true,
+    esbuild: {
+        supported: {
+            'top-level-await': true,
+        },
     },
-  },
-  define: {
-    APP_VERSION: JSON.stringify(process.env.APP_VERSION),
-  },
-  publicDir: './public',
+    define: {
+        APP_VERSION: JSON.stringify(process.env.APP_VERSION),
+    },
+    publicDir: './public',
 });
