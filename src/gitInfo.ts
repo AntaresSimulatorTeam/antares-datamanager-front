9c3cf8927519fca2280bd61fcc58fa3784bc89b0
vargastat <130074062+vargastat@users.noreply.github.com>
2025-06-12 08:36:10 +0200

    fix devin miroir pour package-lock.json (#142)

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-3157_study_page_handle_back_navigation",
-  commit: "85aeaba",
-  commitTime: "2025-06-11T15:11:46.608Z"
+  branch: "fix/devin_miroir",
+  commit: "60f4713",
+  commitTime: "2025-06-12T06:23:54.256Z"
 };
