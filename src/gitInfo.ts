d2779c06979a64d1ce633d9ebfbf9d8102a1c43a
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-13 10:51:49 +0200

    fix: footer height

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "fix/ANT-2760_replace_rds_component_into_customize_one",
-  commit: "b6db672",
-  commitTime: "2025-06-13T07:13:43.746Z"
+  commit: "30ab71b",
+  commitTime: "2025-06-13T08:23:29.305Z"
 };
