6874b6fcc49d98e0653566c58a06ac67c4195929
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-16 14:34:42 +0200

    fix: height shrinking

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "fix/LOAD_warnings_other_area",
-  commit: "7a67dcf",
-  commitTime: "2025-06-16T11:55:08.655Z"
+  commit: "b6ed895",
+  commitTime: "2025-06-16T12:30:41.418Z"
 };
