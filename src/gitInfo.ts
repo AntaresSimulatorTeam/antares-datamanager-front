be7f77bdbeada43a5d0a594cb2f19c9de82b877e
vargastat <tatiana.vargas@rte-france.com>
2025-06-16 15:24:55 +0200

    name change detected in Duplication mode

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-3373_LOAD_wrong_initilization_of_trajectory_after_deletion",
-  commit: "e2b67b7",
-  commitTime: "2025-06-13T15:09:01.843Z"
+  branch: "fix/duplication_name_horizon",
+  commit: "1f7d3e2",
+  commitTime: "2025-06-16T13:24:11.585Z"
 };
