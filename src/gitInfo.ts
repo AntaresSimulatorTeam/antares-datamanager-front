8427d1221a4d38c296bcdb78500285bb3e5afd16
vargastat <tatiana.vargas@rte-france.com>
2025-06-16 13:16:58 +0200

    test ic

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-3373_LOAD_wrong_initilization_of_trajectory_after_deletion",
-  commit: "e2b67b7",
-  commitTime: "2025-06-13T15:09:01.843Z"
+  branch: "fix/both_ic",
+  commit: "10bd057",
+  commitTime: "2025-06-16T11:15:53.393Z"
 };
