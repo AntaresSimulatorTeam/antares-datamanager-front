d3ce7bbb734096f7f9e2628e9ea37a5143b9515a
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-20 12:02:20 +0200

    fix: stdTab test

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-3402_THERMAL_handle_add_row_to_hypothesis_table",
-  commit: "52e4f7c",
-  commitTime: "2025-06-26T09:11:49.130Z"
+  branch: "feat/ANT-3387_THERMAL_initialisation_page",
+  commit: "050431f",
+  commitTime: "2025-06-20T09:03:14.636Z"
 };
