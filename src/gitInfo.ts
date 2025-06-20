25bdf84124c350c9165b0d5a8dbe7732aa2aa9cf
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-20 14:34:04 +0200

    fix: checked icon not hidden

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3387_THERMAL_initialisation_page",
-  commit: "050431f",
-  commitTime: "2025-06-20T09:03:14.636Z"
+  commit: "366b345",
+  commitTime: "2025-06-20T12:00:00.946Z"
 };
