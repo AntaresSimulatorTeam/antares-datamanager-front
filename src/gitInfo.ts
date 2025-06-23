888da101aab79ae88e7570c13e878662a8f9fe57
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-23 15:58:50 +0200

    fix: an change

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3402_THERMAL_handle_add_row_to_hypothesis_table",
-  commit: "83b0cab",
-  commitTime: "2025-06-23T09:23:52.775Z"
+  commit: "e23f081",
+  commitTime: "2025-06-23T12:18:14.948Z"
 };
