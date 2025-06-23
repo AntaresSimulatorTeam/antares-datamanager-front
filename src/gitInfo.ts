3aa905fdb8069c935d99f6c92031eeec6459cc80
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-23 11:20:12 +0200

    fix: alignment in table

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3387_THERMAL_initialisation_page",
-  commit: "0a2e980",
-  commitTime: "2025-06-23T06:40:35.293Z"
+  commit: "5a1df1d",
+  commitTime: "2025-06-23T07:40:34.391Z"
 };
