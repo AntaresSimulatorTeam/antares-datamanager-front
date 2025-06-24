1983e812c0d55b17a51d9a340d6ad911f11a74bf
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-24 19:01:51 +0200

    fix: checked box nested

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3402_THERMAL_handle_add_row_to_hypothesis_table",
-  commit: "9f71ed3",
-  commitTime: "2025-06-23T14:25:00.810Z"
+  commit: "60ba1c4",
+  commitTime: "2025-06-24T11:50:01.782Z"
 };
