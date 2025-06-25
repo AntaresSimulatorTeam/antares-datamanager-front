a68b8d8a8f3439fe782ab6aa4d9995f48d583306
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-25 19:25:45 +0200

    fix: remove row

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3402_THERMAL_handle_add_row_to_hypothesis_table",
-  commit: "60ba1c4",
-  commitTime: "2025-06-24T11:50:01.782Z"
+  commit: "96033bd",
+  commitTime: "2025-06-25T16:53:12.109Z"
 };
