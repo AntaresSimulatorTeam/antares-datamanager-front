f0231b98c029d1a9f996e5f6e3faf70c8ae8c691
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-26 11:20:58 +0200

    fix: divider

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3402_THERMAL_handle_add_row_to_hypothesis_table",
-  commit: "87aeec2",
-  commitTime: "2025-06-26T09:08:56.441Z"
+  commit: "52e4f7c",
+  commitTime: "2025-06-26T09:11:49.130Z"
 };
