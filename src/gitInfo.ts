eddb95e9c133742f0ec498aa722468f983c32983
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-26 11:11:21 +0200

    fix: type unCheckNestedValue

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3402_THERMAL_handle_add_row_to_hypothesis_table",
-  commit: "cda7d8b",
-  commitTime: "2025-06-25T17:27:37.451Z"
+  commit: "87aeec2",
+  commitTime: "2025-06-26T09:08:56.441Z"
 };
