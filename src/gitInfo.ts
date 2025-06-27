47fbce3688bae5b7765d042dc4e538e4db5fa4d1
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-27 09:58:09 +0200

    gif info

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3399_THERMAL_handle_parameters_tab_initialization",
-  commit: "d16c12d",
-  commitTime: "2025-06-27T07:11:47.431Z"
+  commit: "c402e9a",
+  commitTime: "2025-06-27T07:48:47.398Z"
 };
