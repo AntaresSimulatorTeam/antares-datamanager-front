669d9c74d764d14c563b4dc7332199b83b2397fb
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-27 14:20:47 +0200

    fix: divider

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3399_THERMAL_handle_parameters_tab_initialization",
-  commit: "aaf9203",
-  commitTime: "2025-06-27T12:10:26.547Z"
+  commit: "65fc422",
+  commitTime: "2025-06-27T12:20:12.925Z"
 };
