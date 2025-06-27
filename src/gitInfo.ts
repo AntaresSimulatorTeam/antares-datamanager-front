79f463d464dcfccadb9a9e813599297de3ca6f50
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-27 15:23:45 +0200

    fix: border, label header

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3399_THERMAL_handle_parameters_tab_initialization",
-  commit: "65fc422",
-  commitTime: "2025-06-27T12:20:12.925Z"
+  commit: "d4b6f4f",
+  commitTime: "2025-06-27T12:57:54.158Z"
 };
