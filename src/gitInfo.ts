5cefea8b044fb31cb0be1f6f34268b1e442e3c2b
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-27 14:11:11 +0200

    fix: use fetch areas hook

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3399_THERMAL_handle_parameters_tab_initialization",
-  commit: "47fbce3",
-  commitTime: "2025-06-27T11:20:08.656Z"
+  commit: "aaf9203",
+  commitTime: "2025-06-27T12:10:26.547Z"
 };
