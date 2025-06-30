a6dbd013f79a37fdb105ceb89eb54fa45806d816
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-30 12:46:29 +0200

    fix: defaut area

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3399_THERMAL_handle_parameters_tab_initialization",
-  commit: "6cb082f",
-  commitTime: "2025-06-30T08:46:56.098Z"
+  commit: "57799cc",
+  commitTime: "2025-06-30T10:38:51.842Z"
 };
