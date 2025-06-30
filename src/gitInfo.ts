14f6eb7c59a7f9a21b13ab190e4c8f9e77ab4187
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-30 10:17:36 +0200

    fix: checkvalues set

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3484_THERMAL_parameters_add_delete_row",
-  commit: "9c15acf",
-  commitTime: "2025-06-27T14:47:43.354Z"
+  commit: "f6a730a",
+  commitTime: "2025-06-30T07:12:47.876Z"
 };
