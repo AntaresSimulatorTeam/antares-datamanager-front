93016e238544704fec46c09a6ecc32bc3f039a75
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-06-30 15:42:51 +0200

    fix: study id

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3484_THERMAL_parameters_add_delete_row",
-  commit: "f6a730a",
-  commitTime: "2025-06-30T07:12:47.876Z"
+  commit: "6953d09",
+  commitTime: "2025-06-30T12:11:16.885Z"
 };
