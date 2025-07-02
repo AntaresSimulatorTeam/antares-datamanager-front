2e861147e10b4ef0f65eb2909699b531eda088a0
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-07-02 18:40:25 +0200

    fix: empty trajectory linked

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "fix/ANT-3498_refresh_should_keep_active_state_for_tab",
-  commit: "3cc9c1c",
-  commitTime: "2025-07-02T14:51:28.318Z"
+  commit: "18e1167",
+  commitTime: "2025-07-02T15:44:56.923Z"
 };
