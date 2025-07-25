fa8e7a8cce299f38091f5310657b46c99c08d4f8
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-07-25 16:08:01 +0200

    Feat: ANT-3467: Thermal: handle navigation between installed power and parameters

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-3156_project_card_title_clickable",
-  commit: "45edad0",
-  commitTime: "2025-07-18T12:04:21.491Z"
+  branch: "feat/ANT-3467_thermal_navigation_between_installed_power_and_parameters",
+  commit: "39d1af3",
+  commitTime: "2025-07-25T11:09:20.170Z"
 };
