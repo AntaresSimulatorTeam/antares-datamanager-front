3f7ca86852daf13b26ab4e5072e35202ea873919
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-08-27 16:56:06 +0200

    fix: build errors 2

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3536-THERMAL_link_creation_between_trajectory_and_study",
-  commit: "bdc354b",
-  commitTime: "2025-08-27T14:35:44.563Z"
+  commit: "7d9cc1d",
+  commitTime: "2025-08-27T14:55:44.400Z"
 };
