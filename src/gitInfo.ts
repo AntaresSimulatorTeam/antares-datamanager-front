a007e0a9c60bfea9f8622315bf02ec39f6ecaf83
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-02 17:28:17 +0200

    fix: header design

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3515-Design_improve_table_display",
-  commit: "824a6ea",
-  commitTime: "2025-08-22T15:47:55.045Z"
+  commit: "866d4b7",
+  commitTime: "2025-09-02T12:48:01.264Z"
 };
