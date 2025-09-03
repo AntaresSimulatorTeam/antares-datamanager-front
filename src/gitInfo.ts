9a051f6cee712d93884e0d38275b1b081b0ca1a1
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-03 15:34:38 +0200

    Fix: home page - name

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-3515-Design_improve_table_display",
-  commit: "866d4b7",
-  commitTime: "2025-09-02T12:48:01.264Z"
+  branch: "fix/home_page_study_name_should_be_green",
+  commit: "35b8486",
+  commitTime: "2025-09-03T13:29:05.545Z"
 };
