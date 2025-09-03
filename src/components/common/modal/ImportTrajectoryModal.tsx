4d8be32a04c902d7d61e8da8d7f622f09a425190
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-03 15:07:25 +0200

    Fix: rebase develop

--- src/components/common/modal/ImportTrajectoryModal.tsx
+++ src/components/common/modal/ImportTrajectoryModal.tsx
@@ -7,9 +7,7 @@ import { TRAJECTORY_TYPE } from '@/shared/enum/trajectory.ts';
 import { fetchTrajectoriesFromFS } from '@/shared/services/trajectoryService.ts';
 import { convertToFSSelectionOptionType } from '@/shared/utils/formFormatter.ts';
 import StdButton from '@common/base/stdButton/StdButton';
-import { StdIconId } from '@/shared/utils/common/mappings/iconMaps.ts';
 import { OTHER_AREAS } from '@/shared/const/studyConfig.ts';
-import StdButton from '@common/base/stdButton/StdButton.tsx';
 import { StdIconId } from '@/shared/utils/common/mappings/iconMaps.ts';
 
 interface ImportTrajectoryModalProps {
--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3169_project_modify_description_keywords",
-  commit: "8f7fa65",
-  commitTime: "2025-08-29T15:46:22.791Z"
+  commit: "7f40aa6",
+  commitTime: "2025-09-03T13:06:56.703Z"
 };
