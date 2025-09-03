79a0821cb9f794e045a0588ca019e05623094ab7
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-03 16:26:42 +0200

    fix: plus column width

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/home_page_study_name_should_be_green",
-  commit: "35b8486",
-  commitTime: "2025-09-03T13:29:05.545Z"
+  branch: "fix/ANT-3772-Table_display_column_width_should_be_fixed",
+  commit: "151d254",
+  commitTime: "2025-09-03T14:08:21.029Z"
 };
