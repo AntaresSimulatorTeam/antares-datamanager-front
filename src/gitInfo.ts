e8d90fb32a854ebd0ba6c5b775bbed29b183d4a9
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-04 12:19:02 +0200

    Fix: deletable area

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-3772-Table_display_column_width_should_be_fixed",
-  commit: "151d254",
-  commitTime: "2025-09-03T14:08:21.029Z"
+  branch: "fix/default_area_deletable",
+  commit: "264fc4e",
+  commitTime: "2025-09-04T10:03:56.739Z"
 };
