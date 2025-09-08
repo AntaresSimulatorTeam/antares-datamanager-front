48997d8e07c0bf3b4f5a35d55476021d3b524b77
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-08 10:31:45 +0200

    fix build

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3169_project_modify_description_keywords",
-  commit: "7ebeda9",
-  commitTime: "2025-09-05T13:34:33.203Z"
+  commit: "05ebbab",
+  commitTime: "2025-09-08T08:31:13.101Z"
 };
