a52d808a7aef6acc838ea0bfce53ecd25877e5c0
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-10 11:12:08 +0200

    fix: temporary trajectory type

--- src/components/tab/ParametersTab.tsx
+++ src/components/tab/ParametersTab.tsx
@@ -54,7 +54,7 @@ export const ParametersTab = ({ defaultAreas, areas }: ParametersTabProps) => {
   const { hypothesisTrajectories, areasTrajectoryOptions, dropDownListOptions, readOnlyRow } =
     useFetchHypothesisTrajectories(
       study?.id,
-      TRAJECTORY_TYPE.THERMAL_PARAMETER, //TODO : TRAJECTORY_TYPE.THERMAL_TECHNICAL_SPECIFIC_PARAMETER
+      TRAJECTORY_TYPE.THERMAL_CAPACITY, //TODO : TRAJECTORY_TYPE.THERMAL_TECHNICAL_SPECIFIC_PARAMETER
       defaultAreas,
       areas,
       isStudyGenerated,
