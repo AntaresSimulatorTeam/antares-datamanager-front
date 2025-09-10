d837c5e8063d017557cfb6dd26e5704c5b4b1bad
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-10 11:12:08 +0200

    fix: temporary trajectory type

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-3692_thermal_update_parameters_hypothesis_table",
-  commit: "2de05b7",
-  commitTime: "2025-09-10T07:45:36.868Z"
+  branch: "feat/ANT-3796_thermal_parameter_get_trajectories_from_fs",
+  commit: "96def55",
+  commitTime: "2025-09-10T09:07:30.332Z"
 };
