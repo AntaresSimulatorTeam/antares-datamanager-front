9b0a06c1f681aff78996834f08eddf080e069bd5
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-11 11:10:57 +0200

    fix: defautl next to label

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3692_thermal_update_parameters_hypothesis_table",
-  commit: "00773e5",
-  commitTime: "2025-09-10T13:01:54.381Z"
+  commit: "b90a95e",
+  commitTime: "2025-09-11T08:33:41.997Z"
 };
