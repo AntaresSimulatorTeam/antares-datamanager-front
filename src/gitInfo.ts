e0ebd46a5970339cf51ec3054b0a9dfca8014011
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-15 15:18:22 +0200

    fix: spinner color

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3796_thermal_parameter_get_trajectories_from_fs",
-  commit: "b6be6ff",
-  commitTime: "2025-09-15T12:39:58.050Z"
+  commit: "49871fc",
+  commitTime: "2025-09-15T13:16:51.961Z"
 };
