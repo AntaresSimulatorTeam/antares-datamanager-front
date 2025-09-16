defa7fd744763e28f5f46aea752757f67e63df86
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-16 15:12:12 +0200

    fix: git info

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "release/1.4.0",
-  commit: "ed949fa",
-  commitTime: "2025-09-16T12:17:32.129Z"
+  branch: "version/1.4.0",
+  commit: "12b2182",
+  commitTime: "2025-09-16T13:11:47.555Z"
 };
