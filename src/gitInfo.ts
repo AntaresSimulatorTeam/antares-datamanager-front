fa9f64ab8933d7c7f844543b197914d342b708c6
marlenetienne <marlene.etienne@hotmail.fr>
2025-09-16 14:02:29 +0200

    fix: modify expandable colomn size (#197)
    * fix: modify expandable colomn size
    
    * fix: enlarge size
    
    ---------
    
    Co-authored-by: marlenetienne <marlene.etienne_externe@rte-france.com>

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-3816_thermal_common_param_import",
-  commit: "75f4981",
-  commitTime: "2025-09-16T13:40:19.026Z"
+  branch: "fix/column_size",
+  commit: "50d68fe",
+  commitTime: "2025-09-16T09:28:35.873Z"
 };
