38b9312197954b1de552c49dd613341ac5718276
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-16 11:17:24 +0200

    fix: use addrow service

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-3796_thermal_parameter_get_trajectories_from_fs",
-  commit: "b920abb",
-  commitTime: "2025-09-12T13:57:56.512Z"
+  branch: "feat/ANT-3816_thermal_common_param_import",
+  commit: "656863b",
+  commitTime: "2025-09-15T16:02:03.422Z"
 };
