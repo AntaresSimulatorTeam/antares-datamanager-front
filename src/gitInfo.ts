f41b9167e4e544686f250011387ba4215d3ab2f0
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-23 16:22:39 +0200

    Feat: ANT-3776 - Thermal > Parameters > Specific param: import trajectory

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3776_thermal_parameters_specific_params_import",
-  commit: "226391c",
-  commitTime: "2025-09-25T15:50:47.179Z"
+  commit: "31067bd",
+  commitTime: "2025-09-23T13:25:12.467Z"
 };
