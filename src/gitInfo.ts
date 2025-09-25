fb8661170b24e3b1447dd53b21ae432a18b53bea
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-25 18:11:01 +0200

    fix: remove useless height

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3776_thermal_parameters_specific_params_import",
-  commit: "803ad90",
-  commitTime: "2025-09-25T14:21:08.231Z"
+  commit: "226391c",
+  commitTime: "2025-09-25T15:50:47.179Z"
 };
