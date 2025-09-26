630c681e26d129e1735cd0e51ccef175bfcbc767
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-26 16:48:02 +0200

    add french key

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3852_thermal_common_read_only",
-  commit: "40f1d1b9",
-  commitTime: "2025-09-26T14:02:39.131Z"
+  commit: "eb042787",
+  commitTime: "2025-09-26T14:36:34.123Z"
 };
