048632bee2fd686a4bbc6461f5e1cf5717254bad
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-09-30 19:47:54 +0200

    Feat: AnT-3936 - Thermal > specific > trajectory warning display

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3936_specific_trajectory_warning_display",
-  commit: "3b411b5c",
-  commitTime: "2025-10-03T12:59:21.314Z"
+  commit: "6c4ef1ef",
+  commitTime: "2025-09-30T16:01:12.448Z"
 };
