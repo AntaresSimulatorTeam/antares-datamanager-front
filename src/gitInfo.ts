034d31c1c80779f93b8c29260ef4a43997f3c26f
marlenetienne <marlene.etienne@hotmail.fr>
2025-10-01 14:53:23 +0200

    Fix: home page layout (#209)
    * Fix: home page layout
    
    * Fix: remove mock
    
    * fix: justify-between
    
    * fix: project page layout
    
    * fix: reload projects
    
    ---------
    
    Co-authored-by: marlenetienne <marlene.etienne_externe@rte-france.com>

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-3945_home_remote_create_study_button",
-  commit: "6c4ef1ef",
-  commitTime: "2025-10-01T08:48:14.784Z"
+  branch: "fix/table_column_width",
+  commit: "96891905",
+  commitTime: "2025-10-01T11:58:07.223Z"
 };
