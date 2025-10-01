d14fad96aa5b50feb37eb4239a41eb2df270734f
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-10-01 14:28:52 +0200

    fix: reload projects

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "fix/table_column_width",
-  commit: "b114b9d4",
-  commitTime: "2025-10-01T11:54:22.906Z"
+  commit: "96891905",
+  commitTime: "2025-10-01T11:58:07.223Z"
 };
