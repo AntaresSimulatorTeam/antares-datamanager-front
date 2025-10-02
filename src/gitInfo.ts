9421c77aedc41e5402666dfadda9250a6e7951a5
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-10-02 11:29:20 +0200

    Fix: remove google domain

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/table_column_width",
-  commit: "96891905",
-  commitTime: "2025-10-01T11:58:07.223Z"
+  branch: "fix/police",
+  commit: "034d31c1",
+  commitTime: "2025-10-02T09:15:46.054Z"
 };
