32ff864bab7a33bcf43fa454cc64554036d3cc6f
marlenetienne <marlene.etienne@hotmail.fr>
2025-10-02 16:21:32 +0200

    Fix: remove google domain (#211)
    * Fix: remove google domain
    
    * Fix: use system-ui, sans-serif
    
    ---------
    
    Co-authored-by: marlenetienne <marlene.etienne_externe@rte-france.com>

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/table_column_width",
-  commit: "96891905",
-  commitTime: "2025-10-01T11:58:07.223Z"
+  branch: "fix/police",
+  commit: "9421c77a",
+  commitTime: "2025-10-02T13:50:09.697Z"
 };
