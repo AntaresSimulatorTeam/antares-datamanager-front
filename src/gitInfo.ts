2bb01ecfed41c5cc22d546d36ea3bcbe65ded29e
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-10-07 16:37:18 +0200

    Feat: ANT-3976 - Thermal > handle syudy generated

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-ANT-3976_thermal_handle_study_generated",
-  commit: "762bd758",
-  commitTime: "2025-10-09T08:35:42.457Z"
+  commit: "082a4673",
+  commitTime: "2025-10-07T13:32:50.968Z"
 };
