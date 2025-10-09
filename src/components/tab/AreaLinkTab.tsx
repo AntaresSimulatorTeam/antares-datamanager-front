93dfe7253db23977c19e170e9f64b666ce1ccaf6
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-10-09 10:37:14 +0200

    fix: area and link

--- src/components/tab/AreaLinkTab.tsx
+++ src/components/tab/AreaLinkTab.tsx
@@ -114,7 +114,9 @@ export const AreaLinkTab = ({ setErrorMessage }: AreaLinkTabProps) => {
         );
         setReadOnly({
           '0': false,
-          '1': !trajectoryAreaResult || (!trajectoryLinkResult && studyData?.status === StudyStatus.GENERATED),
+          '1':
+            !trajectoryAreaResult.length > 0 ||
+            (!trajectoryLinkResult.length > 0 && studyData?.status === StudyStatus.GENERATED),
         });
       } catch {
         //Silent handler
