9fcc6b473c007fc773767447ab1318e4ece61694
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-10-09 10:37:14 +0200

    fix: area and link

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-ANT-3976_thermal_handle_study_generated",
-  commit: "6f0c80f0",
-  commitTime: "2025-10-09T08:28:33.993Z"
+  commit: "762bd758",
+  commitTime: "2025-10-09T08:35:42.457Z"
 };
