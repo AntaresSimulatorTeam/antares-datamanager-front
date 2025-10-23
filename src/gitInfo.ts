0f8063aa36b3bb5bc89ddb121a60cd084cfced68
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-10-23 09:22:41 +0200

    fix: remove duplicate

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "fix/ANT-3903_specific_param_display_modal",
-  commit: "6594ab21",
-  commitTime: "2025-10-22T07:57:51.103Z"
+  commit: "1e66b7fc",
+  commitTime: "2025-10-23T07:07:52.621Z"
 };
