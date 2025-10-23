629ec8cbb7b8ffe6fd708233db80517ca985006f
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-10-23 14:19:55 +0200

    fix: remove useless condition

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "fix/ANT-3903_specific_param_display_modal",
-  commit: "280d7954",
-  commitTime: "2025-10-23T09:56:59.711Z"
+  commit: "b69c902e",
+  commitTime: "2025-10-23T10:18:13.835Z"
 };
