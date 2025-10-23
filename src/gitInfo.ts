280d79544b56f2273e3607b8fffbe0f1d815d518
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-10-23 11:51:23 +0200

    fix: remove empty line

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "fix/ANT-3903_specific_param_display_modal",
-  commit: "7c6041b1",
-  commitTime: "2025-10-23T07:51:51.021Z"
+  commit: "f9e5cb4f",
+  commitTime: "2025-10-23T09:44:02.349Z"
 };
--- src/hooks/useHypothesisTableRemoveRow.ts
+++ src/hooks/useHypothesisTableRemoveRow.ts
@@ -70,9 +70,12 @@ export const useHypothesisTableRemoveRow = (
                 await unlinkTrajectoryFromStudy(trajectoryIds[0], study.id);
               }
             }
+            const area =
+              trajectoryToDelete?.area ??
+              (type === TRAJECTORY_TYPE.THERMAL_TECHNICAL_SPECIFIC_PARAMETER ? value : row.hypothesis);
             dispatch?.({
               type: STUDY_ACTION.DELETE_TRAJECTORY,
-              payload: { area: trajectoryToDelete?.area ?? row.hypothesis, type },
+              payload: { area, type },
             });
           }
 
