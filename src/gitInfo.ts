d83cf7c909111dd00829e3ea752a846f00b0a420
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-11-03 15:24:23 +0100

    fix: add test unit

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3935_design_use_rem_instaed_of_px",
-  commit: "2b539857",
-  commitTime: "2025-10-24T15:09:55.238Z"
+  commit: "072f13c5",
+  commitTime: "2025-11-03T13:54:16.750Z"
 };
