44d8d45945750b9df79182c088c8bf0074f28405
marlenetienne <marlene.etienne@hotmail.fr>
2025-11-03 11:20:47 +0100

    Feat: ANT-4026 - study creation modal - display error message under field (#219)
    * Feat: study creation modal - display error message under field
    
    * fix: js doc
    
    * fix: remove useless code
    
    * Fix: disabled update
    
    ---------
    
    Co-authored-by: marlenetienne <marlene.etienne_externe@rte-france.com>

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-3903_specific_param_display_modal",
-  commit: "b69c902e",
-  commitTime: "2025-10-23T10:18:13.835Z"
+  branch: "feat/creation_study_modal_normalize_error_message_display",
+  commit: "ed9c9b26",
+  commitTime: "2025-11-03T09:27:30.138Z"
 };
