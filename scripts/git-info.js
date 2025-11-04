12e7963a95894bf64e50607f789af1326b17266b
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-11-04 16:46:27 +0100

    fix: test about é

--- scripts/git-info.js
+++ scripts/git-info.js
@@ -4,6 +4,7 @@ import { writeFileSync } from 'fs';
 try {
   const ref = process.env.GITHUB_REF || '';
   const branch = ref?.replace('refs/heads/', '');
+  console.log('process.env.GITHUB_REF', process.env.GITHUB_REF);
   console.log('branch', branch);
   const commit = execSync('git rev-parse --short HEAD').toString().trim();
   const commitTime = new Date().toISOString();
