c8e1372cd06c99363914b4a64c290224b0d989df
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-11-04 15:13:39 +0100

    fix test avatar

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-3935_design_use_rem_instaed_of_px",
-  commit: "e3f43e97",
-  commitTime: "2025-11-04T13:50:53.669Z"
+  commit: "5bc94751",
+  commitTime: "2025-11-04T14:13:19.763Z"
 };
