28df4e8d5adbf0cdf1a13be5ed90844451c48ea1
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-11-12 15:35:39 +0100

    feat: ANT-4010 - Thermal: parameters > handle import costs

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-4070_thermal_parameters_specific_display_area_linked",
-  commit: "5f981ee4",
-  commitTime: "2025-11-07T14:46:44.304Z",
+  branch: "feat/ANT-4010_thermal_parameters_costs_handle_import",
+  commit: "706a715c",
+  commitTime: "2025-11-12T08:30:21.156Z",
 };
