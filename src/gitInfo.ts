f972a867a7f6c5dd95d3d9242e6cf95cab740326
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-11-13 11:52:09 +0100

    Feat: ANT-4105 - Thermal > Parameters > select trajectory from BDD

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-4010_thermal_parameters_costs_handle_import",
-  commit: "fbdd4cc7",
-  commitTime: "2025-11-12T15:58:39.377Z",
+  branch: "feat/ANT-4105_thermal_parameters_selct_trajectory_from_BDD",
+  commit: "f92bd73a",
+  commitTime: "2025-11-13T10:14:20.654Z",
 };
