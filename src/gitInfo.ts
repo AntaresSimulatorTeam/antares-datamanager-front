080a62b9999df1ca974271f3f4b86ed2028f5c79
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-11-27 10:32:59 +0100

    Feat: ANT-4231 - Attach hook: remove useless GEt request

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-4105_thermal_parameters_selct_trajectory_from_BDD",
-  commit: "c6f51331",
-  commitTime: "2025-11-24T13:48:31.728Z",
+  branch: "feat/ANT-4231_attach_hook_remove_useless_request",
+  commit: "61e33125",
+  commitTime: "2025-11-27T09:29:50.728Z",
 };
