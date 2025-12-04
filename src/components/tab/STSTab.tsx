a1661ce944cfeea729174b565156448a150c74f4
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-04 16:36:06 +0100

    Feat: ANT-4169 - Thermal + STS: handle display table when study is generated

--- src/components/tab/STSTab.tsx
+++ src/components/tab/STSTab.tsx
@@ -57,26 +57,6 @@ const STSTab = ({ defaultAreas, areas }: TabProps) => {
     }
   }, [isStudyGenerated]);
 
-  useEffect(() => {
-    if (isStudyGenerated) {
-      setIsStudyGenerated(true);
-      const newData = filterNestedRow(data);
-      setData(newData);
-      const rows = generateReadOnlyIndexMap(data);
-      setReadOnly(rows);
-    }
-  }, [isStudyGenerated]);
-
-  useEffect(() => {
-    if (isStudyGenerated) {
-      setIsStudyGenerated(true);
-      const newData = filterNestedRow(data);
-      setData(newData);
-      const rows = generateReadOnlyIndexMap(data);
-      setReadOnly(rows);
-    }
-  }, [isStudyGenerated]);
-
   const handleSelectionChange = useCallback(
     async (value: string, isChecked: boolean) => {
       const indexRow = data.findIndex((row) => row.hypothesis === value);
