d7b30f7ba3c1c38d4e4ede85164ab484e763a55f
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-04 16:36:06 +0100

    Feat: ANT-4169 - Thermal + STS: handle display table when study is generated

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-4169_sts_generated_study",
-  commit: "d1415268",
-  commitTime: "2025-12-05T13:18:42.354Z",
+  commit: "6a51545b",
+  commitTime: "2025-12-04T13:22:15.516Z",
 };
