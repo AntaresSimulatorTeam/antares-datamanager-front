4f0e1c25c08dea2c587fb0ea4b20d828639fa20d
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-05 14:54:10 +0100

    fix: useffect when detach

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-4169_sts_generated_study",
-  commit: "cf8e7613",
-  commitTime: "2025-12-05T08:41:34.474Z",
+  commit: "d1415268",
+  commitTime: "2025-12-05T13:18:42.354Z",
 };
