c315dc4b5f26f354b76b757fb2a398e4c7500f7f
marlenetienne <marlene.etienne@hotmail.fr>
2025-12-05 11:33:50 +0100

    fix: status in STS (#237)
    Co-authored-by: marlenetienne <marlene.etienne_externe@rte-france.com>

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-4096_STS_Capacity_Add_delete_area_table",
-  commit: "f7af7b3d",
-  commitTime: "2025-12-03T15:19:24.121Z",
+  branch: "fix/sts_status_2",
+  commit: "399161ac",
+  commitTime: "2025-12-05T10:22:22.615Z",
 };
