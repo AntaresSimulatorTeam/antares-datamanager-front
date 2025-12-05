b3cb753ff2d5a0e02b3cea4a6c48570cec2c941e
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-05 09:44:41 +0100

    fix: status

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-4169_sts_generated_study",
-  commit: "6a51545b",
-  commitTime: "2025-12-04T13:22:15.516Z",
+  commit: "cf8e7613",
+  commitTime: "2025-12-05T08:41:34.474Z",
 };
