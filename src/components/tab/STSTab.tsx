184f5c1c9aadd5e6aea4a00fe2faea6dc5815fc2
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-09 16:46:56 +0100

    Fix: ANT-4267 - Handle route with path

--- src/components/tab/STSTab.tsx
+++ src/components/tab/STSTab.tsx
@@ -30,7 +30,13 @@ const STSTab = ({ defaultAreas, areas, studyData }: TabProps) => {
     studyState.studyStatus === StudyStatus.GENERATED || studyData.status === StudyStatus.GENERATED,
   );
   const { hypothesisTrajectories, areasTrajectoryOptions, dropDownListOptions, readOnlyRow, technologyList } =
-    useFetchHypothesisTrajectories(areas, studyData?.id, TRAJECTORY_TYPE.STS, defaultAreas, isStudyGenerated);
+    useFetchHypothesisTrajectories(
+      areas,
+      studyData?.id,
+      TRAJECTORY_TYPE.STS,
+      defaultAreas,
+      isStudyGenerated
+    );
   const { removeRow } = useHypothesisTableRemoveRow(studyData, dispatch, setData, setCheckedValues);
 
   useEffect(() => {
