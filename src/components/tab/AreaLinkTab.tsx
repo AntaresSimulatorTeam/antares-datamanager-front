9c182a078a62786893fd2e9c8a29fd825db9965e
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-11 16:10:38 +0100

    fix: retrieve link trajectory

--- src/components/tab/AreaLinkTab.tsx
+++ src/components/tab/AreaLinkTab.tsx
@@ -80,7 +80,6 @@ export const AreaLinkTab = ({ setErrorMessage, studyData }: AreaLinkTabProps) =>
 
   useEffect(() => {
     setErrorMessage('');
-    console.log('================== hypothesisTrajectories', hypothesisTrajectories);
     hypothesisTrajectories && setData(hypothesisTrajectories);
     readOnlyRow && setReadOnly(readOnlyRow);
   }, [hypothesisTrajectories, readOnlyRow, setErrorMessage, studyData?.id]);
