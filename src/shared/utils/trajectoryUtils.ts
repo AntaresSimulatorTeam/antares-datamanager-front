b1028484d7222e0f03c7b68f67d7c673a2c9026d
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-18 14:32:05 +0100

    Fix: filter row mehtod

--- src/shared/utils/trajectoryUtils.ts
+++ src/shared/utils/trajectoryUtils.ts
@@ -431,8 +431,8 @@ export const filterRow = (data: HypothesisRowData[]): HypothesisRowData[] =>
         row.isDefault || // 🔹 garde toujours les isDefault
         !row.isDeletable ||
         row.status === TRAJECTORY_SELECTION_STATUS.OK ||
-        row.status === TRAJECTORY_SELECTION_STATUS.MISSING ||
-        (row.status === TRAJECTORY_SELECTION_STATUS.ERROR && row.subRows && row.subRows.length > 0),
+        (row.status === TRAJECTORY_SELECTION_STATUS.MISSING && row.subRows && row.subRows.length > 0),
+      //(row.status === TRAJECTORY_SELECTION_STATUS.ERROR && row.subRows && row.subRows.length > 0),
     );
 
 /**
