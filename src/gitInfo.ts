b667763755f536ca0f2e60ddd88073a20439ebce
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-24 17:37:17 +0100

    Feat: ANT-4323_projects_extract_unpinnedprojects_from_projects

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-4323_project_page_handle_pinned_projects",
-  commit: "bf9e2fae",
-  commitTime: "2025-12-29T10:36:18.740Z",
+  commit: "22114cef",
+  commitTime: "2025-12-24T16:35:19.161Z",
 };
