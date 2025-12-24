cc854f803b4053953ce19effafb9bee77a7bc399
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-24 17:37:17 +0100

    Feat: ANT-4323_projects_extract_unpinnedprojects_from_projects

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-4350_thermal_param_modulation",
-  commit: "d0156b68",
-  commitTime: "2026-01-06T09:10:06.242Z",
+  branch: "feat/ANT-4323_project_page_handle_pinned_projects",
+  commit: "22114cef",
+  commitTime: "2025-12-24T16:35:19.161Z",
 };
