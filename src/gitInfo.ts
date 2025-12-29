7a425bde077c29ac23cf8400e713db7da758e951
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-29 11:29:29 +0100

    fix: use std dropdown component

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-4323_project_page_handle_pinned_projects",
-  commit: "22114cef",
-  commitTime: "2025-12-24T16:35:19.161Z",
+  commit: "aa2a4ef3",
+  commitTime: "2025-12-29T10:17:59.277Z",
 };
