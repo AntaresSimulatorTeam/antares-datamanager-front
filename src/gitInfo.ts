5a7ee1dfa1a92176b91ac5ff617a67ff2d901111
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-29 11:36:58 +0100

    fix: unit tests

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-4323_project_page_handle_pinned_projects",
-  commit: "aa2a4ef3",
-  commitTime: "2025-12-29T10:17:59.277Z",
+  commit: "bf9e2fae",
+  commitTime: "2025-12-29T10:36:18.740Z",
 };
