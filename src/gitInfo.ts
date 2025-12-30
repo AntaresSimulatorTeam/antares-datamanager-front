36464b5d69492ca76d2d8fde7320d7459c76c8b0
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-30 11:16:41 +0100

    Feat: ANT-4321 - Project modal: error display

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
   branch: "feat/ANT-4323_project_page_handle_pinned_projects",
-  commit: "22114cef",
-  commitTime: "2025-12-24T16:35:19.161Z",
+  commit: "2dc510a9",
+  commitTime: "2025-12-30T08:23:36.877Z",
 };
