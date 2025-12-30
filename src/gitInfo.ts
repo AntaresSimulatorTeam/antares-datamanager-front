c0d3427cdd345c560afb81346760bfa2d811763e
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-30 16:29:24 +0100

    Feat: ANT-4329 - Project page: breadcrumb and banner

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-4321_project_modal_error_display",
-  commit: "9e569b38",
-  commitTime: "2025-12-31T13:44:54.079Z",
+  branch: "feat/ANT-4329_project_page_breadcrumb_and_banner",
+  commit: "2e086ec3",
+  commitTime: "2025-12-30T14:45:07.509Z",
 };
