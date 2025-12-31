04e0a1ee1576a4840848b6b2a4bcac5d0e7fa3b6
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-31 16:32:53 +0100

    Feat: ANT-4331 - Navbar color / study table font size

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-4321_project_modal_error_display",
-  commit: "2e086ec3",
-  commitTime: "2025-12-31T08:54:45.123Z",
+  branch: "feat/ANT-4331_navbar_and_study_table",
+  commit: "de71d7f3",
+  commitTime: "2025-12-31T14:30:13.183Z",
 };
