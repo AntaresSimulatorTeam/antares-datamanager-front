de71d7f33bf1d1c58394cae18c15f3c50e995f43
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-31 11:03:26 +0100

    fix: keywords

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-4329_project_page_breadcrumb_and_banner",
+  branch: "feat/ANT-4321_project_modal_error_display",
   commit: "2e086ec3",
-  commitTime: "2025-12-30T14:45:07.509Z",
+  commitTime: "2025-12-31T08:54:45.123Z",
 };
