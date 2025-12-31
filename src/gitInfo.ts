ce4e99f34b75bc22741db82e256157a3da6d2daf
marlenetienne <marlene.etienne_externe@rte-france.com>
2025-12-31 14:12:41 +0100

    fix: nb max characters

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-4329_project_page_breadcrumb_and_banner",
-  commit: "878bbe39",
-  commitTime: "2025-12-31T10:14:41.260Z",
+  branch: "feat/ANT-4321_project_modal_error_display",
+  commit: "b02acb23",
+  commitTime: "2025-12-31T12:09:42.465Z",
 };
