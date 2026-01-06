dbe8f2c00a092f07b867a88d7e5c7094faf6f40a
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-01-06 17:02:41 +0100

    chore: bump version to v1.4.4

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-4355_thermal_hypothesis_table_scroll_bar",
-  commit: "3e51a9b0",
-  commitTime: "2026-01-06T15:21:05.620Z",
+  branch: "release/1.4.4",
+  commit: "9aeeb93e",
+  commitTime: "2026-01-06T15:58:24.492Z",
 };
