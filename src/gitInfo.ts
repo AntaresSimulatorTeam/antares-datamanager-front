e1bf3eed9d4d927ee75fa825f1a05167fb13f9f2
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-01-06 16:22:23 +0100

    Fix: ANT-4355 - Thermal > hypothesis table scroll bar

--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "feat/ANT-4331_navbar_and_study_table",
-  commit: "8abd7653",
-  commitTime: "2026-01-05T15:26:04.431Z",
+  branch: "fix/ANT-4355_thermal_hypothesis_table_scroll_bar",
+  commit: "3e51a9b0",
+  commitTime: "2026-01-06T15:21:05.620Z",
 };
