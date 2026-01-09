ca5e9db693c5443a7b30ed4657e8c6c71a42b549
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-01-09 11:17:51 +0100

    fix: branch avec variable env

--- scripts/git-info.js
+++ scripts/git-info.js
@@ -3,8 +3,9 @@ import { writeFileSync, readFileSync } from 'fs';
 
 try {
   const head = readFileSync('.git/HEAD', 'utf8').trim(); // HEAD peut contenir : "ref: refs/heads/develop"
-  const branch = head.startsWith('ref:') ? head.replace('ref: refs/heads/', '') : head;
+  //const branch = head.startsWith('ref:') ? head.replace('ref: refs/heads/', '') : head;
   //const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+  const branch = process.env.BUILD_VERSION;
   const commit = execSync('git rev-parse --short HEAD').toString().trim();
   const commitTime = new Date().toISOString();
 
--- src/gitInfo.ts
+++ src/gitInfo.ts
@@ -1,5 +1,5 @@
 export const GIT_INFO = {
-  branch: "fix/ANT-4269_about_retrieve_branch_name",
-  commit: "238cb38a",
-  commitTime: "2026-01-09T09:45:58.827Z",
+  branch: "",
+  commit: "72cc57e9",
+  commitTime: "2026-01-09T09:57:19.749Z",
 };
