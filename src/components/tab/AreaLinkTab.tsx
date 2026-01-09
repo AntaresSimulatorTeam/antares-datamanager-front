59aae4a1fd0945d5e6be3d5a46ad3e8e65c7a5c0
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-01-09 17:36:27 +0100

    fix: use data info

--- src/components/tab/AreaLinkTab.tsx
+++ src/components/tab/AreaLinkTab.tsx
@@ -219,9 +219,7 @@ export const AreaLinkTab = ({ setErrorMessage, studyData }: AreaLinkTabProps) =>
   const handleTrajectoryUpdate = async (rowId: string, value: string, status: RowStatus) => {
     const rowIndex = Number(rowId);
     const trajectory = data[rowIndex]?.trajectory;
-    const type = rowIndex === 0 ? TRAJECTORY_TYPE.AREA : TRAJECTORY_TYPE.LINK;
-    const dbTrajectory =
-      dbTrajectories.find((item: DbTrajectory) => item.trajectoryName === value && item.type === type) ?? trajectory;
+    const dbTrajectory = dbTrajectories.find((item: DbTrajectory) => item.trajectoryName === value) ?? trajectory;
     try {
       if (dbTrajectory?.id != null && status === 'success') {
         setErrorMessage('');
