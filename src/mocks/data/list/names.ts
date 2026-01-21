f1b40999c6176bb986b93c5d839a53565c31d26f
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-01-21 13:52:03 +0100

    add unit test and detach

--- src/mocks/data/list/names.ts
+++ src/mocks/data/list/names.ts
@@ -222,6 +222,6 @@ export const ThermalOptions = [
   'TAC',
 ];
 
-export const STSTechnology = ['Battery', 'DSR', 'EV', 'PSP'];
+export const STSTechnology = ['battery', 'DSR', 'EV', 'PSP'];
 
 export const Technologies = ThermalOptions.concat(STSTechnology);
