9407882a094b8a2fa0b91a6d94674325cd05be2c
marlenetienne <marlene.etienne@hotmail.fr>
2026-01-28 17:26:47 +0100

    Fix: api /db (#268)
    * Fix: api /db
    
    * Fix: 2
    
    ---------
    
    Co-authored-by: marlenetienne <marlene.etienne_externe@rte-france.com>

--- src/shared/services/trajectoryService.ts
+++ src/shared/services/trajectoryService.ts
@@ -57,8 +57,8 @@ export const fetchTrajectoriesFromDB = async (
   const queryParams = new URLSearchParams({
     trajectoryType,
     horizon,
-    ...(options?.area && { area: options.area }),
-    ...(options?.technology && { technology: options.technology }),
+    area: options?.area ?? '',
+    technology: options?.technology ?? '',
     ...(options?.fileNameContains && { fileNameContains: options.fileNameContains }),
   }).toString();
   const urlApi = `${TRAJECTORY_DATA_BASE_ENDPOINT}?${queryParams}`;
