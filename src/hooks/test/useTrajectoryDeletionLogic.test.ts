d539bd98f9de685bb31ca6ac6587b548dc2928fd
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-02-25 18:32:37 +0100

    fix: add unit test

--- src/hooks/test/useTrajectoryDeletionLogic.test.ts
+++ src/hooks/test/useTrajectoryDeletionLogic.test.ts
@@ -18,7 +18,6 @@ vi.mock('@/shared/utils/trajectoryUtils.ts', () => ({
 vi.mock('@/shared/helpers/hypothesisTableHelper.ts', () => ({
   findSpecificTrajectoryToDelete: vi.fn(),
   getSpecificTrajectories: vi.fn(),
-  shouldDeleteCapacityModulation: vi.fn(),
   collectTrajectoriesRecursively: vi.fn(),
 }));
 
