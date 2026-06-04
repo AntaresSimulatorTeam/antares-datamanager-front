380658298a4068a3e903f7e1825d59226d285fdc
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-06-04 18:40:13 +0200

    Fix: select button 2

--- src/components/input/SelectAndSearchableInput.tsx
+++ src/components/input/SelectAndSearchableInput.tsx
@@ -122,7 +122,7 @@ const SelectAndSearchableInput = ({
           flexDirection: 'column',
           minWidth: width,
         }}
-        className={`[&_[data-label-position][data-error]]:!w-[${width ?? '350px'}]`}
+        className={`[&_[data-label-position][data-error]]:!w-[${width ?? '300px'}]`}
       >
         <TextInput
           id="text-input-select"
