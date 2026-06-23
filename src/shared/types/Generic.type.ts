3ec1705324c75df5cf9764d3f18a065a23d4ea61
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-06-23 10:46:51 +0200

    Feat: ANT-5051 - Hydro PSP: generated study - display table

--- src/shared/types/Generic.type.ts
+++ src/shared/types/Generic.type.ts
@@ -97,6 +97,7 @@ export const isTrajectorySubrowsType = (value: unknown): value is TrajectoryWith
     TRAJECTORY_TYPE.RES_LOAD,
     TRAJECTORY_TYPE.RES_TECHNOLOGY_DISTRIBUTION,
     TRAJECTORY_TYPE.HYDRO_SERIES,
+    TRAJECTORY_TYPE.HYDRO_PSP_SERIES,
   ].includes(value as TrajectoryWithSubRowsType);
 
 export const isTrajectoryResType = (value: unknown): value is TrajectoryWithSubRowsType =>
