4e8a55babbc00433e31a5ac01a80aff61424c3b7
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-07-02 11:47:18 +0200

    Feat: ANT-4856: DS > Tab : add tab component

--- src/components/pegase/pegaseCard/pegaseCardContent/PegaseCardContent.tsx
+++ src/components/pegase/pegaseCard/pegaseCardContent/PegaseCardContent.tsx
@@ -1,9 +1,9 @@
 import StdTagList from '@common/base/StdTagList/StdTagList.tsx';
 import { formatDateToDDMMYYYY } from '@/shared/utils/dateFormatter.ts';
-import StdAvatar from '@common/layout/stdAvatar/StdAvatar.tsx';
 import { avatarCase } from '@/shared/utils/textUtils.ts';
 import { ProjectInfo } from '@/shared/types';
 import { useTranslation } from 'react-i18next';
+import StdAvatar from '@common/layout/stdAvatar/StdAvatar.tsx';
 
 export type PegaseCardContentProps = {
   project: ProjectInfo;
--- src/shared/utils/trajectoryUtils.ts
+++ src/shared/utils/trajectoryUtils.ts
@@ -4,7 +4,6 @@ import {
   isTrajectoryHydroType,
   isTrajectoryResType,
   RowStatus,
-  StdTabItemProps,
   TechnologyType,
 } from '@/shared/types';
 import { TRAJECTORY_SELECTION_STATUS, TRAJECTORY_TYPE } from '@/shared/enum/trajectory.ts';
@@ -40,8 +39,8 @@ import {
   TRAJECTORY_THERMAL_MODULATION_PARAMETER_IMPORT,
   TRAJECTORY_THERMAL_SPECIFIC_PARAMETER_IMPORT,
 } from '@/shared/const/apiEndPoint.ts';
-import { StdIconId } from '@/shared/utils/common/mappings/iconMaps.ts';
 import { HypothesisType, SearchParams } from '@/shared/types/HypothesisTable.ts';
+import { TabItemProps } from '@design-system-rte/core/components/tab/tab.interface';
 
 /**
  * Get trajectory status from row status
@@ -587,64 +586,88 @@ export const addNestedRow = (
  * Return all tabs available for a study configuration
  * @param {TFunction<'translation', undefined>} t - Translation function
  * @param {boolean} isTrajectoryAreaLinked - Flag to indicate if an AREA trajectory is linked to the study
- * @return {StdTabItemProps[]} - Array of tab data model
+ * @return {TabItemProps[]} - Array of tab data model
  */
-export const getStudyMenu = (t: (value: string) => string, isTrajectoryAreaLinked: boolean): StdTabItemProps[] => [
+export const getStudyMenu = (t: (value: string) => string, isTrajectoryAreaLinked: boolean): TabItemProps[] => [
   {
     id: TRAJECTORY_TYPE.AREA,
     panelId: TRAJECTORY_TYPE.AREA,
     label: t('studyDetails.@areas_links'),
-    icon: StdIconId.LinkedServices, //'share',
+    icon: 'share',
     disabled: false,
+    badgeType: 'brand',
+    badgeContent: 'number',
+    showBadge: true,
   },
   {
     id: TRAJECTORY_TYPE.LOAD,
     panelId: TRAJECTORY_TYPE.LOAD,
     label: t('studyDetails.@load'),
-    icon: StdIconId.Monitoring, //'monitoring',
+    icon: 'monitoring',
     disabled: isTrajectoryAreaLinked,
+    badgeType: 'brand',
+    badgeContent: 'number',
+    showBadge: true,
   },
   {
     id: TRAJECTORY_TYPE.THERMAL_CAPACITY,
     panelId: TRAJECTORY_TYPE.THERMAL_CAPACITY,
     label: t('studyDetails.@thermal'),
-    icon: StdIconId.LocalFireDepartment, //'fire',
+    icon: 'fire',
     disabled: isTrajectoryAreaLinked,
+    badgeType: 'brand',
+    badgeContent: 'number',
+    showBadge: true,
   },
   {
     id: TRAJECTORY_TYPE.STS,
     panelId: TRAJECTORY_TYPE.STS,
     label: t('studyDetails.@sts'),
-    icon: StdIconId.BatteryChargingFull, //'battery-charging-full',
+    icon: 'battery-charging-full',
     disabled: true,
+    badgeType: 'brand',
+    badgeContent: 'number',
+    showBadge: true,
   },
   {
     id: TRAJECTORY_TYPE.DSR,
     panelId: TRAJECTORY_TYPE.DSR,
     label: t('studyDetails.@dsr'),
-    icon: StdIconId.InkEraser, //'eraser',
+    icon: 'eraser',
     disabled: true,
+    badgeType: 'brand',
+    badgeContent: 'number',
+    showBadge: true,
   },
   {
     id: TRAJECTORY_TYPE.MISC_CAPACITY,
     panelId: TRAJECTORY_TYPE.MISC_CAPACITY,
     label: t('studyDetails.@misc'),
-    icon: StdIconId.Category, //'category',
+    icon: 'category',
     disabled: true,
+    badgeType: 'brand',
+    badgeContent: 'number',
+    showBadge: true,
   },
   {
     id: TRAJECTORY_TYPE.RES_CAPACITY,
     panelId: TRAJECTORY_TYPE.RES_CAPACITY,
     label: t('studyDetails.@res'),
-    icon: StdIconId.EnergySavingsLeaf, //'eco',
+    icon: 'eco',
     disabled: true,
+    badgeType: 'brand',
+    badgeContent: 'number',
+    showBadge: true,
   },
   {
     id: TRAJECTORY_TYPE.HYDRO_SERIES,
     panelId: TRAJECTORY_TYPE.HYDRO_SERIES,
     label: t('studyDetails.@hydro'),
-    icon: StdIconId.Water, //'water',
+    icon: 'water',
     disabled: true,
+    badgeType: 'brand',
+    badgeContent: 'number',
+    showBadge: true,
   },
 ];
 
