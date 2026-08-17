1dc3777233a63f50587eb1041ef67cbbae274e72
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-08-17 10:56:53 +0200

    fix: front plugin

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,17 @@
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { defineConfig } from 'vite';
-import topLevelAwait from 'vite-plugin-top-level-await';
 var DEFAULT_PORT = 8080;
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        topLevelAwait({
-            // The export name of top-level await promise for each chunk module
-            promiseExportName: '__tla',
-            // The function to generate import names of top-level await promise in each chunk module
-            promiseImportName: function (i) { return "__tla_".concat(i); },
-        }),
+        // topLevelAwait({
+        //   // The export name of top-level await promise for each chunk module
+        //   promiseExportName: '__tla',
+        //   // The function to generate import names of top-level await promise in each chunk module
+        //   promiseImportName: (i) => `__tla_${i}`,
+        // }),
     ],
     build: {
         target: 'esnext',
