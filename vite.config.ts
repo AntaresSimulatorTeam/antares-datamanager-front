ee4dde24ccd2d9884543c9d96d329713bc7aa67a
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-08-17 11:30:39 +0200

    fix: remove top level await

--- vite.config.ts
+++ vite.config.ts
@@ -14,12 +14,6 @@ const DEFAULT_PORT = 8080;
 export default defineConfig({
   plugins: [
     react(),
-    // topLevelAwait({
-    //   // The export name of top-level await promise for each chunk module
-    //   promiseExportName: '__tla',
-    //   // The function to generate import names of top-level await promise in each chunk module
-    //   promiseImportName: (i) => `__tla_${i}`,
-    // }),
   ],
   build: {
     target: 'esnext',
