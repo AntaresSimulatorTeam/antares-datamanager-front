b3caf04e6e8312f7387e82ebefdf47c67eedf076
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-08-28 12:42:28 +0200

    add change horizon 2

--- src/components/tab/AreaLinkTab.tsx
+++ src/components/tab/AreaLinkTab.tsx
@@ -75,7 +75,7 @@ export const AreaLinkTab = ({ studyData }: AreaLinkTabProps) => {
       { type: TRAJECTORY_TYPE.ADEQUACY_PATCH, labelKey: t('settings.@adequacyPatch') },
       { type: TRAJECTORY_TYPE.FLOWBASED, labelKey: t('settings.@flowBased'), options: {hasRecalculateOption: true} },
       { type: TRAJECTORY_TYPE.SETTINGS, labelKey: t('settings.@title'), subRows: [
-          { type: TRAJECTORY_TYPE.SETTINGS, labelKey: t('settings.@generalData') }]
+        { type: TRAJECTORY_TYPE.SETTINGS, labelKey: t('settings.@generalData') }]
         //{ type: TRAJECTORY_TYPE.SCENARIO_BUILDER, labelKey: t('settings.@scenarioBuilder') }]
       },
     ]],
