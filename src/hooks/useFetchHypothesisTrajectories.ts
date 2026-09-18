6e160a91d646507c3622d76580b7de2753d9a607
marlenetienne <marlene.etienne_externe@rte-france.com>
2026-09-18 11:48:39 +0200

    remove check nuclear

--- src/hooks/useFetchHypothesisTrajectories.ts
+++ src/hooks/useFetchHypothesisTrajectories.ts
@@ -203,8 +203,8 @@ export const useFetchHypothesisTrajectories = (
             isStudyGenerated,
             defaultAreaListNotInList,
           });
-          const areasWithTrajectoryOK = areasWithTrajectory.filter(trajectory => trajectory.trajectoryName?.length > 0);
-          const { areaOptions, checkedValues } = buildCheckListBox(areasWithTrajectoryOK, areas, defaultAreas);
+
+          const { areaOptions, checkedValues } = buildCheckListBox(areasWithTrajectory, areas, defaultAreas);
 
           results = rawResults.flatMap((result) => {
             if (result.trajType === hydroTypeToSet) {
